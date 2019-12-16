d4e8b6e65d57b639f5456312a98553fe76b0ae34
WMXPY <wm@mengw.io>
2019-12-16 10:24:57 -0600

    release object buffer version

--- test/unit/object.test.ts
+++ test/unit/object.test.ts
@@ -19,4 +19,34 @@ describe('Given a {ObjectBuffer} Class', (): void => {
 
         expect(buffer).to.be.instanceOf(ObjectBuffer);
     });
+
+    it('should be able to add object', (): void => {
+
+        const key: string = chance.string();
+        const value: string = chance.string();
+
+        const buffer: ObjectBuffer = ObjectBuffer.create();
+
+        buffer.add(key, value);
+
+        expect(buffer.build()).to.be.deep.equal({
+            [key]: value,
+        });
+    });
+
+    it('should be able to add object if exist', (): void => {
+
+        const key: string = chance.string();
+        const key2: string = chance.string();
+        const value: string = chance.string();
+
+        const buffer: ObjectBuffer = ObjectBuffer.create();
+
+        buffer.addIfExist(key, value);
+        buffer.addIfExist(key2, undefined);
+
+        expect(buffer.build()).to.be.deep.equal({
+            [key]: value,
+        });
+    });
 });

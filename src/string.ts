f06d4694cd7b71da6e9e19c04089f988648f7c0f
WMXPY <wm@mengw.io>
2019-12-16 15:17:02 -0600

    implement string buffer

---
+++ src/string.ts
@@ -0,0 +1,52 @@
+/**
+ * @author WMXPY
+ * @namespace Buffer
+ * @description String
+ */
+
+export class StringBuffer {
+
+    public static create(): StringBuffer {
+
+        return new StringBuffer();
+    }
+
+    private _buffer: string[];
+
+    private constructor() {
+
+        this._buffer = [];
+    }
+
+    public addIfExist(value?: string): this {
+
+        if (value === undefined || value === null) {
+            return this;
+        }
+        return this.add(value);
+    }
+
+    public add(value: string): this {
+
+        this._buffer.push(value);
+        return this;
+    }
+
+    public build(): string {
+
+        return this._buffer.join('');
+    }
+
+    public clean(): this {
+
+        this._buffer = [];
+        return this;
+    }
+
+    public flush(): string {
+
+        const result: string = this.build();
+        this.clean();
+        return result;
+    }
+}

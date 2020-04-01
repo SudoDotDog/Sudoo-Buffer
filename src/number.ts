b951daa4b002d2e5831a7babdd8c0708c6dcf7d5
WMXPY <wm@mengw.io>
2020-04-01 14:21:30 -0500

    add number buffer step

--- src/number.ts
+++ src/number.ts
@@ -12,6 +12,7 @@ export class NumberBuffer {
     }
 
     private _buffer: number;
+    private _step: number;
 
     private constructor(initial?: number) {
 
@@ -20,29 +21,52 @@ export class NumberBuffer {
         } else {
             this._buffer = 0;
         }
+        this._step = 1;
     }
 
     public get value(): number {
 
         return this._buffer;
     }
+    public get step(): number {
 
-    public addIfExist(value?: number): this {
-
-        if (value === undefined || value === null) {
-            return this;
-        }
-        return this.add(value);
+        return this._step;
     }
 
-    public add(value: number): this {
+    public setStep(step: number): this {
 
-        this._buffer += value;
+        this._step = step;
+        return this;
+    }
+
+    public resetStep(): this {
+
+        this._step = 1;
+        return this;
+    }
+
+    public add(step?: number): this {
+
+        this._buffer += this._getStep(step);
+        return this;
+    }
+
+    public minus(step?: number): this {
+
+        this._buffer -= this._getStep(step);
         return this;
     }
 
     public getValue(): number {
 
         return this._buffer;
+    }
+
+    private _getStep(step?: number): number {
+
+        if (typeof step === 'number') {
+            return step;
+        }
+        return this._step;
     }
 }

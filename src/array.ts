edbae2c2dc64f3ebe5a3b78c6f0821c56d4bfcfc
WMXPY <wm@mengw.io>
2020-04-14 13:19:56 -0500

    implement array buffer

---
+++ src/array.ts
@@ -0,0 +1,63 @@
+/**
+ * @author WMXPY
+ * @namespace Buffer
+ * @description Array
+ */
+
+export type ArrayBufferVerifyFunction<T> = (element: T) => boolean;
+export class ArrayBuffer<T> {
+
+    public static create<T>(initial: T[] = []): ArrayBuffer<T> {
+
+        return new ArrayBuffer<T>(initial);
+    }
+
+    private readonly _initial: T[];
+
+    private _buffer: T[];
+
+    private constructor(initial: T[]) {
+
+        this._initial = initial;
+        this._buffer = initial;
+    }
+
+    public get length(): number {
+        return this._buffer.length;
+    }
+
+    public addIfExist(value?: T): this {
+
+        if (value === undefined || value === null) {
+            return this;
+        }
+        return this.add(value);
+    }
+
+    public addIfValid(value: T, verifyFunction: ArrayBufferVerifyFunction<T>): this {
+
+        if (!verifyFunction(value)) {
+            return this;
+        }
+        return this.add(value);
+    }
+
+    public add(value: T): this {
+
+        this._buffer.push(value);
+        return this;
+    }
+
+    public reset(): this {
+
+        this._buffer = this._initial;
+        return this;
+    }
+
+    public flush(): T[] {
+
+        const value: T[] = this._buffer;
+        this.reset();
+        return value;
+    }
+}
